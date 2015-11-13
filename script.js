b5196b3cc60301881b07b9883df6013b1222a009
ecupaio <ecupaio@gmail.com>
2015-11-13 13:34:49 -0500

    no message

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-$(document).ready(function(){
-   $("#page1").removeClass("show-hide");
-});
-$('#navigation a').on('click', function(e) {
-    e.preventDefault();
-    var index = $('a').index(this) + 1;
-    
-    $('.show-hide').each(function(){
-     
-        if($(this).attr('rel') == index){
-            $(this).addClass('active');
-            $(this).show();
-        }else{
-            $(this).removeClass('active');
-            $(this).hide();
-        }
+$(function(){
+$('.carousel').carousel({
+      interval: false,
     });
-    
+$('.carousel-control.right').trigger('click');
 });
